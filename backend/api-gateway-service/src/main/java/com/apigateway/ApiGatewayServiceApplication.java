b654a4454f4f1ce00b2258a3d6a2705cd96e79ad
Neha <2799nehajha@gmail.com>
2021-07-23 22:22:59 +0530

    API Gateway Server Running

---
+++ backend/api-gateway-service/src/main/java/com/apigateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+	@GetMapping("/check")
+    public String list() {
+        return "Succesful hit";
+    }
+}
